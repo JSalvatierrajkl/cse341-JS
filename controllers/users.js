3a816f353163e8051738155500b3fb8e836741d1
JSalvatierrajkl <joeljsalvatierra@gmail.com>
2025-09-06 16:31:34 -0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -4,8 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async(req, res) =>{
     const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'applications/json');
-        res.status(200).json(users);
+        res.status(200).json(users); 
     });
 
 };
@@ -14,7 +13,6 @@ const getSingle = async(req, res) =>{
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
     result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'applications/json');
         res.status(200).json(users[0]);
     });
 
